f3e9105e5d49fbe2e376a381a918aa57901edb07
Nguyen Trung Quan <trungquanjr123@gmail.com>
2023-10-19 17:09:19 +0700

    Class + Widget ListView

---
+++ signature_scent/lib/baitap/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:signature_scent/baitap/items/itemclass.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: const ItemClass(items: 10));
+  }
+}
